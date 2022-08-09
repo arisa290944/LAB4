ca74220517432b9879baa0a7a7291d950c805387
arisa290944 <aripattha@kkumail.com>
2022-08-09 20:13:34 +0700

    Up

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'LAB4'`, () => {
+  it(`should have as title 'LAB3'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('LAB4');
+    expect(app.title).toEqual('LAB3');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('LAB4 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('LAB3 app is running!');
   });
 });
